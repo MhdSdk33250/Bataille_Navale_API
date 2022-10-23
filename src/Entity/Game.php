d259e497b8d590b5098d6a3de5b1f8c328a8baef
Seddik Mehdi <mhdi.seddik@gmail.com>
2022-10-23 19:47:12 +0200

    :sparkles: added game class

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GameRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GameRepository::class)]
@@ -21,6 +23,14 @@ class Game
 
     #[ORM\Column]
     private ?int $numberOfBoats = null;
+
+    #[ORM\OneToMany(mappedBy: 'game', targetEntity: Player::class)]
+    private Collection $players;
+
+    public function __construct()
+    {
+        $this->players = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +69,36 @@ class Game
     public function setNumberOfBoats(int $numberOfBoats): self
     {
         $this->numberOfBoats = $numberOfBoats;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Player>
+     */
+    public function getPlayers(): Collection
+    {
+        return $this->players;
+    }
+
+    public function addPlayer(Player $player): self
+    {
+        if (!$this->players->contains($player)) {
+            $this->players->add($player);
+            $player->setGame($this);
+        }
+
+        return $this;
+    }
+
+    public function removePlayer(Player $player): self
+    {
+        if ($this->players->removeElement($player)) {
+            // set the owning side to null (unless already changed)
+            if ($player->getGame() === $this) {
+                $player->setGame(null);
+            }
+        }
 
         return $this;
     }
