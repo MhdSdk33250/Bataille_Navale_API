0b866ba748ba4b843c2bede5fc316c52d103789d
Seddik Mehdi <mhdi.seddik@gmail.com>
2022-10-23 20:21:31 +0200

    :lock: Fix player register
    You can now register a new player without jwt

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -22,11 +22,14 @@ class PlayerController extends AbstractController
     protected PlayerRepository $playerRepository;
     protected EntityManagerInterface $em;
 
-    public function __construct(ManagerRegistry $doctrine){
+    public function __construct(ManagerRegistry $doctrine)
+    {
         $this->em = $doctrine->getManager();
         $this->playerRepository = $doctrine->getRepository(Player::class);
     }
-
+    /**
+     * Get all players
+     */
     #[Route('/api/players', name: 'get.players', methods: ['GET'])]
     public function getPlayers(SerializerInterface $serializer): JsonResponse
     {
@@ -34,17 +37,21 @@ class PlayerController extends AbstractController
         $jsonPlayers = $serializer->serialize($players, 'json');
         return new JsonResponse($jsonPlayers, Response::HTTP_OK, ['accept' => 'json'], true);
     }
-
+    /**
+     * Get player found by id
+     */
     #[Route('/api/player/{idPlayer}', name: 'player.get', methods: ['GET'])]
-    #[ParamConverter("player", options: ["id" => "idPlayer"], class:'App\Entity\Player')]
+    #[ParamConverter("player", options: ["id" => "idPlayer"], class: 'App\Entity\Player')]
     public function player(Player $player, SerializerInterface $serializer): JsonResponse
     {
         $jsonPlayer = $serializer->serialize($player, 'json');
         return new JsonResponse($jsonPlayer, Response::HTTP_OK, ['accept' => 'json'], true);
     }
-
+    /**
+     * Delete player by id
+     */
     #[Route('/api/player/{idPlayer}', name: 'player.delete', methods: ['DELETE'])]
-    #[ParamConverter("player", options: ["id" => "idPlayer"], class:'App\Entity\Player')]
+    #[ParamConverter("player", options: ["id" => "idPlayer"], class: 'App\Entity\Player')]
     public function deletePlayer(Player $player): JsonResponse
     {
         $this->playerRepository->remove($player);
@@ -54,26 +61,30 @@ class PlayerController extends AbstractController
         ]);
         return new JsonResponse($jsonResponse, Response::HTTP_NO_CONTENT);
     }
-
+    /**
+     * Edit player by id
+     */
     #[Route('/api/player/{idPlayer}', name: 'player.edit', methods: ['PATCH'])]
-    #[ParamConverter("player", options: ["id" => "idPlayer"], class:'App\Entity\Player')]
+    #[ParamConverter("player", options: ["id" => "idPlayer"], class: 'App\Entity\Player')]
     public function updatePlayer(Request $request, Player $player, EntityManagerInterface $em, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
-        $updatePlayer = $serializer->deserialize($request->getContent(),Player::class,'json',[AbstractNormalizer::OBJECT_TO_POPULATE => $player]);
+        $updatePlayer = $serializer->deserialize($request->getContent(), Player::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $player]);
 
         $this->em->persist($updatePlayer);
         $this->em->flush();
-        $jsonPlayer = $serializer->serialize($updatePlayer,'json',['groups'=>'getPlayers']);
-        $location = $urlGenerator->generate('player.get',['idPlayer' => $player->getId()],UrlGeneratorInterface::ABSOLUTE_URL);
+        $jsonPlayer = $serializer->serialize($updatePlayer, 'json', ['groups' => 'getPlayers']);
+        $location = $urlGenerator->generate('player.get', ['idPlayer' => $player->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
 
-        return new JsonResponse($jsonPlayer, Response::HTTP_CREATED,["Location" => $location],true);
+        return new JsonResponse($jsonPlayer, Response::HTTP_CREATED, ["Location" => $location], true);
     }
-
-    #[Route('/api/player', name: 'player.create', methods: ['POST'])]
+    /**
+     * Register new player
+     */
+    #[Route('/register', name: 'player.create', methods: ['POST'])]
     public function createPlayer(Request $request, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
-        
-        $player = $serializer->deserialize($request->getContent(),Player::class,'json');
+
+        $player = $serializer->deserialize($request->getContent(), Player::class, 'json');
         $plaintextPassword = $player->getPassword();
         // $errors = $validator->validate($player); // TODO Bryan
         // if($errors->count() > 0) {
@@ -87,9 +98,8 @@ class PlayerController extends AbstractController
         $player->setPassword($hashedPassword);
         $this->em->persist($player);
         $this->em->flush();
-        $jsonPlayer = $serializer->serialize($player,'json');
-        $location = $urlGenerator->generate('player.get',['idPlayer' => $player->getId()],UrlGeneratorInterface::ABSOLUTE_URL);
-        return new JsonResponse($jsonPlayer, Response::HTTP_CREATED,["Location" => $location],true);
-        
+        $jsonPlayer = $serializer->serialize($player, 'json');
+        $location = $urlGenerator->generate('player.get', ['idPlayer' => $player->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse($jsonPlayer, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 }
