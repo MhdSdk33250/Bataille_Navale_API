889d81c421b8db55a4b525265fa3c9a1b9eb0825
Seddik Mehdi <mhdi.seddik@gmail.com>
2022-10-28 09:28:58 +0200

    add doc

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
